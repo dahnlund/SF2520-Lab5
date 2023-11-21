7518de3ba7b9aaf76c46c07d2896a081f71f85e2
dahnlund <david.ahnlund@gmail.com>
2023-11-21 15:45:14 +0100

    Updated code

--- julia/part2.jl
+++ julia/part2.jl
@@ -4,17 +4,18 @@ using SparseArrays
 using Plots
 using IterativeSolvers
 using IncompleteLU
+using Preconditioners
 
-"Part 2c"
-mat = matopen("data/convdiff.mat")
-#mat = matopen("data/cooling_flange.mat")
+"Part 2"
+#mat = matopen("data/convdiff.mat")
+mat = matopen("data/cooling_flange.mat")
 A = read(mat)["A"]
 close(mat)
 N = length(A[:,1])
-
-
 b = sprand(N,1.0,rand,Float64)
 
+TOL = 1e-4
+K = 1000;
 #Linear Solve
 t = time()
 x_ls = A\Vector(b);
@@ -23,6 +24,19 @@ time_ls = time()-t
 err_ls = norm(A*x_ls-b)/norm(b)
 println("Computation time Linear Solver: $time_ls. RELRES: $err_ls")
 
+
+# Regular PCG
+t = time()
+x_regpcg, RESVEC_regpcg = minres(A,b, false, false, 1e-4, 1e-4, K, true, false);
+time_ls = time()-t
+err_ls = norm(A*x_regpcg-b)/norm(b)
+println("Computation time regular pcg: $time_regpcg. RELRES: $err_regpcg")
+
+
+
+
+
+"""
 # GMRES
 LU = ilu(A, τ = 0.1)
 t = time()
@@ -43,3 +57,4 @@ println("Computation time BiCG (without ilu): $time_gm. RELRES: $err_gm")
 plot!(RESVEC)
 display(plot1)
 
+"""
