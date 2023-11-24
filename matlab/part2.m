9e46f21396a0fd72fd2e6b744bafa08fdca8b95c
David Ahnlund <dahnlund@kth.se>
2023-11-24 17:07:01 +0100

    fixed matlab part

--- matlab/part2.m
+++ matlab/part2.m
@@ -6,6 +6,7 @@ A = mat.("A");
 
 N = length(A(:,1));
 b = rand(N,1);
+norm_b = norm(b);
 
 %% Linear Solver
 tic
@@ -30,18 +31,18 @@ time_pcgd = toc();
 fprintf("pcg M=diag::      Computation time: %.04f seconds. RELRES: %.05d, Iterations: %.0f, Flag: %.0f\n", ...
     time_pcgd, RELRES, ITER, FLAG)
 
-%% pcg, M as ichog
+%% pcg, M as ichol
 
 L = ichol(A);
 tic()
 [x_pcgc,FLAG,RELRES,ITER,RESVEC_pcgc] = pcg(A, b, 1e-04, 10000, L, L');
 time_pcgc = toc();
-fprintf("pcg M=diag::      Computation time: %.04f seconds. RELRES: %.05d, Iterations: %.0f, Flag: %.0f\n", ...
+fprintf("pcg M=ichol::      Computation time: %.04f seconds. RELRES: %.05d, Iterations: %.0f, Flag: %.0f\n", ...
     time_pcgc, RELRES, ITER, FLAG)
 
 
 %% Compare convergence
-semilogy(RESVEC_cg);hold on; semilogy(RESVEC_pcgd); semilogy(RESVEC_pcgc)
+semilogy(RESVEC_cg/norm_b);hold on; semilogy(RESVEC_pcgd/norm_b); semilogy(RESVEC_pcgc/norm_b)
 xlabel("Iterations")
 ylabel("RELRES")
 legend("PCG", "PCG (diag)", "PCG (ichol)")
@@ -53,6 +54,7 @@ A = mat.("A");
 
 N = length(A(:,1));
 b = rand(N,1);
+norm_b = norm(b);
 
 %% Solve with GE
 tic
@@ -64,14 +66,18 @@ fprintf("Linear Solver::      Computation time: %.04f seconds. RELRES: %.05d\n",
 
 %% Using GMRES
 
-M = ilu(A);
+[L,U] = ilu(A);
 tic()
-[x_gm,FLAG,RELRES,ITER,RESVEC_gm] = gmres(A, b, false,1e-4, 1000, M);
+[x_gm,FLAG,RELRES,ITER,RESVEC_gm] = gmres(A, b, N, 1e-5, 3000, L,U);
 time_gm = toc();
-fprintf("\nGMRES::     Computation time: %.04f seconds. RELRES: %.05d, Iterations: %.0f, Flag: %.0f\n",time_gm, RELRES, ITER, FLAG)
-semilogy(RESVEC_gm)
+fprintf("GMRES::  Computation time: %.04f seconds. RELRES: %.05d, Iterations: %.01f, Flag: %.01f\n",time_gm, RELRES, ITER(2), FLAG)
+semilogy(RESVEC_gm/norm_b); hold on
 
-
+tic()
+[x_gmw,FLAGw,RELRESw,ITERw,RESVEC_gmw] = gmres(A, b, N, 1e-5, 3000);
+time_gmw = toc();
+fprintf("GMRES no precond.::  Computation time: %.04f seconds. RELRES: %.05d, Iterations: %.01f, Flag: %.01f\n",time_gmw, RELRESw, ITERw(2), FLAGw)
+semilogy(RESVEC_gmw/norm_b)
 
 
 
